3d7f6c2a140278c71ffb5852134baaff87ea133e
Rodrick Nfinyoh <mernxl@gmail.com>
2022-07-22 04:40:49 +0100

    fix logging in application

--- event_imager/main.py
+++ event_imager/main.py
@@ -1,3 +1,4 @@
+import sys
 from concurrent import futures
 import logging
 
@@ -9,14 +10,16 @@ from event_imager_service import EventImagerService
 
 
 def serve():
-    # enable logging with INFO level
-    logging.basicConfig(level=logging.INFO)
+    # enable logging with INFO level @see https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, stream=sys.stdout
+    )
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     event_imager_pb2_grpc.add_EventImagerServicer_to_server(EventImagerService(), server)
 
     server.add_insecure_port(f"{config['server_host']}:{config['server_port']}")
-    print(f"Running GRPC Server at {config['server_host']}:{config['server_port']}")
+    logging.info(f"Running GRPC Server at {config['server_host']}:{config['server_port']}")
     server.start()
     server.wait_for_termination()
 
