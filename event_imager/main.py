ed09a55018b155c2d6035bdca3db793f7f257e24
Rodrick Nfinyoh <mernxl@gmail.com>
2022-07-22 04:05:02 +0100

    fix missed types

--- event_imager/main.py
+++ event_imager/main.py
@@ -11,8 +11,8 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     event_imager_pb2_grpc.add_EventImagerServicer_to_server(EventImagerService(), server)
 
-    server.add_insecure_port(f"{config.env.config['server_host']}:{config.env.config['server_port']}")
-    print(f"Running GRPC Server at {config.env.config['server_host']}:{config.env.config['server_port']}")
+    server.add_insecure_port(f"{config['server_host']}:{config['server_port']}")
+    print(f"Running GRPC Server at {config['server_host']}:{config['server_port']}")
     server.start()
     server.wait_for_termination()
 
